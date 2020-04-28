1da56e791daffe2afab01bf6d284ccd0fe3743ee
Shamel Lakin <christlife4u@gmail.com>
2020-04-28 14:20:50 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -3,8 +3,8 @@ class Teacher < User
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
   def teach
+    binding.pry
      KNOWLEDGE.first.to_s #return random elemnet from array 
-     binding.pry
   end 
   
 end
